a55862eeee554d51fee2205c9f4ff748c5acffd1
Amankothari043 <86096147+Amankothari043@users.noreply.github.com>
2021-06-19 12:17:37 +0530

    Add files via upload

--- Bsc(IT)2B/AmanKothari-2023025/OperatingSystem/AmanKothari-2023025.c
+++ Bsc(IT)2B/AmanKothari-2023025/OperatingSystem/AmanKothari-2023025.c
@@ -1,1 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int n,bt[20],wt[20],tat[20],avrwt=0,avrtat=0,i,j;
+    printf("Enter total number of processes(maximum 20):");
+    scanf("%d",&n);
 
+    printf("\nEnter Process Burst Time\n");
+    for(i=0;i<n;i++)
+    {
+        printf("P[%d]:",i+1);
+        scanf("%d",&bt[i]);
+    }
+
+    wt[0]=0;
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+    }
+
+    printf("\nProcess\t\tBurst Time\tWaiting Time\tTurnaround Time");
+
+    for(i=0;i<n;i++)
+    {
+        tat[i]=bt[i]+wt[i];
+        avrwt+=wt[i];
+        avrtat+=tat[i];
+        printf("\nP[%d]\t\t%d\t\t%d\t\t%d",i+1,bt[i],wt[i],tat[i]);
+    }
+
+    avrwt/=i;
+    avrtat/=i;
+    printf("\n\nAverage Waiting Time:%d",avrwt);
+    printf("\nAverage Turnaround Time:%d",avrtat);
+
+    return 0;
+}
